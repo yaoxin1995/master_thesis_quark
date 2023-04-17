fdda448a48d44bf61ed3a07b1d868abbb2d7ea66
yaoxin1 <yaoxin.jing1@huawei.com>
2023-04-17 22:26:58 +0200

    suport shared libs measurement

--- qkernel/src/shield/software_measurement_manager.rs
+++ qkernel/src/shield/software_measurement_manager.rs
@@ -6,9 +6,12 @@ use qlib::addr::Addr;
 use crate::qlib::kernel::task::Task;
 use crate::qlib::loader::Process;
 use qlib::auxv::AuxEntry;
-
+use qlib::path::*;
+use qlib::linux_def::*;
+use fs::file::File;
 
 const APP_NMAE: &str = "APPLICATION_NAME"; 
+const SHARED_LIB_PATTERN: &str = ".so"; 
 
 lazy_static! {
     pub static ref SOFTMEASUREMENTMANAGER:  RwLock<SoftwareMeasurementManager> = RwLock::new(SoftwareMeasurementManager::default());
@@ -52,6 +55,17 @@ fn get_application_name(envs : Vec<String>) -> Option<String>{
     }
 
     None
+}
+
+pub fn is_shared_lib(file_name: &str) -> Result<bool> {
+
+    let base_name = Base(&file_name).to_string();
+
+    let is_shared_lib = base_name.contains(SHARED_LIB_PATTERN);
+
+    info!("is_shared_lib file_name {:?}, is_shared_lib {:?}", file_name, is_shared_lib);
+
+    Ok(is_shared_lib)
 }
 
 impl SoftwareMeasurementManager {
@@ -170,8 +184,36 @@ impl SoftwareMeasurementManager {
         Ok(())
     }
 
-    pub fn measure_shared_lib(&mut self) -> Result<()> {
+    pub fn measure_shared_lib(&mut self, start_addr: u64, file: &File, task: &Task, fixed: bool, mmmap_len: u64) -> Result<()> {
 
+        let uattr = file.UnstableAttr(task)?;
+        let shared_lib_size = uattr.Size;
+
+        // let length = match Addr(shared_lib_size).RoundDown() {
+        //     Err(_) => return Err(Error::SysError(SysErr::ENOMEM)),
+        //     Ok(l) => l.0,
+        // };
+        debug!("measure_shared_lib, addr {:x}, shared_lib_size {:x}, fixed {:?}, mmmap_len {:x}", start_addr, shared_lib_size, fixed, mmmap_len);
+        
+        let data: Result<Vec<u8>>;
+        if fixed {
+            let length = match Addr(mmmap_len).RoundDown() {
+                Err(_) => return Err(Error::SysError(SysErr::ENOMEM)),
+                Ok(l) => l.0,
+            };
+
+            data = task.CopyInVec(start_addr, length as usize);
+        } else {
+            data = task.CopyInVec(start_addr, shared_lib_size as usize);
+        }
+
+        if data.is_err() {
+            info!("measure_shared_lib After task.CopyInVec got error {:?}", data);
+            return Err(data.err().unwrap());
+        }
+
+        let loadable = data.unwrap();
+        self.updata_measurement(loadable).unwrap();
         Ok(())
     }
 
